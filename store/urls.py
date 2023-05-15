9b35fd9a165139706e6186d34d5f53f6d8a3d06e
ramyo564 <yohan032yohan@gmail.com>
2023-05-16 06:44:32 +0900

    complete s7

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.store, name='store'),
     path('<str:category_slug>/', views.store, name='products_by_category'),
+    path('<str:category_slug>/<str:product_slug>/', views.product_detail, name='product_detail'),
     # path('category/<slug:category_slug>/', views.store, name='products_by_category'),
     # path('category/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
     # path('search/', views.search, name='search'),
