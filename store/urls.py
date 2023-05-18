5f41ed172ed12e911bf00f2c1c7e58177678fc0e
ramyo564 <yohan032yohan@gmail.com>
2023-05-18 17:59:10 +0900

    Complete s11

--- store/urls.py
+++ store/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.store, name='store'),
-    path('<str:category_slug>/', views.store, name='products_by_category'),
-    path('<str:category_slug>/<str:product_slug>/', views.product_detail, name='product_detail'),
+    path('category/<str:category_slug>/', views.store, name='products_by_category'),
+    path('category/<str:category_slug>/<str:product_slug>/', views.product_detail, name='product_detail'),
     # path('category/<slug:category_slug>/', views.store, name='products_by_category'),
     # path('category/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
-    # path('search/', views.search, name='search'),
+    path('search/', views.search, name='search'),
     # path('submit_review/<int:product_id>/', views.submit_review, name='submit_review'),
 ]
