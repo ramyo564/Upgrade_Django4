80c12ec3efca504085dd5bbd93e134548603109b
ramyo564 <yohan032yohan@gmail.com>
2023-05-21 17:36:14 +0900

    fix cart bug

--- carts/views.py
+++ carts/views.py
@@ -53,6 +53,9 @@ def add_cart(request, product_id):
         if product_variation in ex_var_list:
             # increase the cart item quantity
             index = ex_var_list.index(product_variation)
+            # print(f'index {index}')
+            # print(f'ex_var_list : {ex_var_list} / ex_var_list_type : {type(ex_var_list)}')
+            # print(f'product_variation : {product_variation} / product_variation : {type(product_variation)}')
             item_id = id[index]
             item = CartItem.objects.get(product=product, id=item_id)
             item.quantity += 1
@@ -96,10 +99,10 @@ def remove_cart(request, product_id, cart_item_id):
         pass
     return redirect('cart')
         
-def remove_cart_item(request, product_id):
+def remove_cart_item(request, product_id, cart_item_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart=cart)  
+    cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)  
     cart_item.delete()
     return redirect('cart')
     
