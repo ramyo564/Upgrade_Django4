0b23aa89e34f7427e8b5b83cc8d076a9397bd220
ramyo564 <yohan032yohan@gmail.com>
2023-05-26 11:30:31 +0900

    complete s17 장바구니 삭제 및 수량조절 버튼 수정

--- accounts/views.py
+++ accounts/views.py
@@ -10,6 +10,10 @@ from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 from django.http import HttpResponse
+
+
+from carts.views import _cart_id
+from carts.models import Cart, CartItem
 # Create your views here.
 
 def register(request):
@@ -63,7 +67,49 @@ def login(request):
         email = request.POST['email']
         password = request.POST['password']
         user = auth.authenticate(email=email, password=password)
+        
         if user is not None:
+            
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+                    
+
+                    # Getting the product variations by cart id 
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+                        
+                    # Get the cart items from the user to access his product variations
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)                    
+                
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                            
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+                    
+            except:
+
+                pass
             auth.login(request, user)
             messages.success(request, "You are new logged in.")
             return redirect('dashboard')
