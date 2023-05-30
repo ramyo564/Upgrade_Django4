09d815b5093d392deb15c7cdfa6b203ebbb43aa3
ramyo564 <yohan032yohan@gmail.com>
2023-05-30 19:18:51 +0900

    complete s23 review rating

--- store/models.py
+++ store/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from category.models import Category
 from django.utils.text import slugify
 from django.urls import reverse
+from accounts.models import Account
+from django.db.models import Avg, Count
 
 # Create your models here.
 class Product(models.Model):
@@ -25,7 +27,21 @@ class Product(models.Model):
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
         self.slug = slugify(self.product_name, allow_unicode=True)
         super().save(force_insert, force_update, using, update_fields)
-        
+    
+    def averageReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(average=Avg('rating'))
+        avg = 0
+        if reviews['average'] is not None:
+            avg = float(reviews['average'])
+        return avg
+
+    def countReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(count=Count('id'))
+        count = 0
+        if reviews['count'] is not None:
+            count = int(reviews['count'])
+        return count
+    
 class VariationManager(models.Manager):
     def colors(self):
         return super(VariationManager, self).filter(variation_category='color', is_active=True)
@@ -49,3 +65,17 @@ class Variation(models.Model):
 
     def __str__(self):
         return self.variation_value
+    
+class ReviewRating(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100, blank=True)
+    review = models.TextField(max_length=500, blank=True)
+    rating = models.FloatField()
+    ip = models.CharField(max_length=20, blank=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subject
