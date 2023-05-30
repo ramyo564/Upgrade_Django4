14952afc56c1b5136637f0c15fcd190915a6f1db
ramyo564 <yohan032yohan@gmail.com>
2023-05-30 23:07:14 +0900

    complete dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -15,7 +15,7 @@ from django.http import HttpResponse
 from carts.views import _cart_id
 from carts.models import Cart, CartItem
 import requests
-
+from orders.models import Order
 
 # Create your views here.
 
@@ -164,7 +164,14 @@ def activate(reqeust, uidb64, token):
     
 @login_required(login_url = 'login')
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered=True)
+    orders_count = orders.count()
+    
+    context = {
+        'orders_count': orders_count,
+    }
+    return render(request, 'accounts/dashboard.html', context)
+   
 
 
 def forgotPassword(request):
