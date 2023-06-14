fbc4e46f09bf0d2012fdfbbd0e750b988a4dfc46
ramyo564 <yohan032yohan@gmail.com>
2023-06-14 17:37:59 +0900

    finish S3

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG',default=True ,cast=bool)
 
 ALLOWED_HOSTS = ['django-greatkart-env-7.eba-fdvmcepg.ap-northeast-2.elasticbeanstalk.com']
-
+# ALLOWED_HOSTS = ['django-greatkart-env-7.eba-fdvmcepg.ap-northeast-2.elasticbeanstalk.com',"*"]
 
 # Application definition
 
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     "phonenumber_field",
     "orders",
     "admin_honeypot",
+    'storages',
+    
 ]
 
 MIDDLEWARE = [
@@ -116,6 +118,9 @@ else:
     }
 
 
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -150,11 +155,32 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /'static'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR /'static'
+# STATICFILES_DIRS = [
+#     'greatkart/static',
+# ]
+
+# AWS S3 Static Files Configuration
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static'
+
 STATICFILES_DIRS = [
     'greatkart/static',
 ]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'greatkart.media_storages.MediaStorage'
+
 
 # media files configuration
 MEDIA_URL = '/media/'
