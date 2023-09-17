f3d61b4ee549ee546060e267ee8e8275931873e6
ramyo564 <yohan032yohan@gmail.com>
2023-09-18 00:41:32 +0900

    Local settings - add secretkey

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "f)nmw*cl$s)&b-mk(mrpfom78i-j&+1x8&t5@wnxb15a=bglst"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
